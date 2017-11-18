ce3f446bdf7234479b1a17b00213599a908978f6
callumscalcium <33298204+callumscalcium@users.noreply.github.com>
2017-11-18 16:11:48 +0000

    wfoeihweiofef

--- bms.cpp
+++ bms.cpp
@@ -7,6 +7,10 @@ BatteryManagementSystem::BatteryManagementSystem()
 /// TASK 9 (BOTH) //////////////////////////////////////////////////////////////
 double BatteryManagementSystem::get_voltage()
 {
-  return 0;
+  return 96;
+}
+double BatteryManagementSystem::get_current()
+{
+  return 97;
 }
 /// END OF TASKS 9AB ///////////////////////////////////////////////////////////
